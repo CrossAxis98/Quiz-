86f2591e925ffe327ee2805a0df20b601a32ad5b
CrossAxis98 <roberto2611@gmail.com>
2020-05-16 00:12:46 +0200

    First fragment implementation

--- app/src/main/java/com/arianameble/quiz/FirstFragment.java
+++ app/src/main/java/com/arianameble/quiz/FirstFragment.java
@@ -1,15 +1,62 @@
 package com.arianameble.quiz;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class FirstFragment extends Fragment {
+
+    private static final String TAG = "FirstFragment";
+
+    private static final int FURNITURES_IN_QUIZ = 10;
+
+    private List<String> fileNameLists;
+
+    private List<String> quizFurnituresList;
+
+    private Set<String> categoriesSet;
+
+    private String correctAnswer;
+
+    private int totalGuessess;
+
+    private int correctAnswers;
+
+    private int guessRows;
+
+    private SecureRandom random;
+
+    private Handler handler;
+
+    private Animation shakeAnimation;
+
+    private LinearLayout quizLinearLayout;
+
+    private TextView questionNumberTextView;
+
+    private ImageView furnitureImageView;
+
+    private LinearLayout[] guessLinearLayouts;
+
+    private TextView answerTextView;
 
     @Override
     public View onCreateView(
@@ -17,8 +64,41 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+       super.onCreateView(inflater, container, savedInstanceState);
+
+       View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+       fileNameLists = new ArrayList<>();
+       quizFurnituresList = new ArrayList<>();
+       random = new SecureRandom();
+       handler = new Handler();
+
+       shakeAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.incorrect_shake);
+       shakeAnimation.setRepeatCount(3);
+
+       quizLinearLayout = (LinearLayout) view.findViewById(R.id.quizLinearLayout);
+       questionNumberTextView = (TextView) view.findViewById(R.id.questionNumberTextView);
+       furnitureImageView = (ImageView) view.findViewById(R.id.furnitureImageView);
+       guessLinearLayouts = new LinearLayout[4];
+       guessLinearLayouts[0] = (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+       guessLinearLayouts[1] = (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+       guessLinearLayouts[2] = (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+       guessLinearLayouts[3] = (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+       answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+
+       for(LinearLayout row: guessLinearLayouts) {
+           for (int column = 0; column < row.getChildCount(); column++) {
+               Button button = (Button) row.getChildAt(column);
+               button.setOnClickListener(guessButtonListener);
+           }
+       }
+
+       questionNumberTextView.setText(getString(R.string.question, 1, FURNITURES_IN_QUIZ));
+
+       return view;
     }
+
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
--- app/src/main/java/com/arianameble/quiz/MainActivity.java
+++ app/src/main/java/com/arianameble/quiz/MainActivity.java
@@ -1,6 +1,9 @@
 package com.arianameble.quiz;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import
         android.os.Bundle;
 
@@ -9,8 +12,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -39,26 +42,56 @@ public class MainActivity extends AppCompatActivity {
 
         androidx.preference.PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferenceChangeListener);
 
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE) || screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+        {
+            phoneDevice = false;
+        }
+
+       // if(phoneDevice) {
+         //   setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //}
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        if(preferencesChanged)
+        {
+            FirstFragment quizFragment = (FirstFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        int orientation = getResources().getConfiguration().orientation;
+
+        if(orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferenceIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferenceIntent);
 
         return super.onOptionsItemSelected(item);
     }
